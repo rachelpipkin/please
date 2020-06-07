251a5d7a673461379ea6fd3e9792a81dcb5166d9
Rachel Pipkin <rachelpipkin@gmail.com>
2020-06-06 21:16:35 -0700

    send assigned task message

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const { WebClient } = require("@slack/web-api");
+const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
+
+const emptyTask = {
+  assignedID: "",
+  requesterID: "",
+  text: "",
+};
+const task = Object.assign({}, emptyTask);
+const userIDRegex = /<@U[0-9A-Z]*\>/;
+
 app.listen(port, () =>
   console.log(`Please app listening at http://localhost:${port}`)
 );
 app.use(express.json());
-app.use(errorHandler);
-
-// bot token auth
-// const { WebClient } = require("@slack/web-api");
-// const token = process.env.SLACK_TOKEN;
-// const web = new WebClient(token);
+app.use(routeErrorHandler);
 
 // routes
 app.post("/slack-events", function (req, res) {
@@ -19,42 +26,61 @@ app.post("/slack-events", function (req, res) {
 
   if (data.event) {
     const event = data.event;
-    handleMessage(event.text);
+    createRequest(event.text, event.user);
   }
 
   res.send(data.challenge);
 });
 
 // methods
-function errorHandler(err, req, res, next) {
-  res.status(500);
-  res.send("error", { error: err });
-}
+function createRequest(text, user) {
+  task.assignedID = getUserID(text);
+  task.requesterID = user;
 
-function handleMessage(text) {
-  const userID = getUserID(text);
-
-  if (!userID) {
+  if (task.assignedID == "") {
     // TODO handle missing user on request
     console.log("Request User to assign to here...");
   } else {
-    // getTaskText(text);
-    // TODO handle sending request to the user
-    console.log("Gonna send a request to userid: ", userID);
+    task.text = getTaskText(text);
+    sendTaskToAssigned();
   }
 }
 
+function getTaskText(text) {
+  const userIDRaw = text.match(userIDRegex);
+  const taskText = text.replace(userIDRaw, "");
+
+  return taskText.trim();
+}
+
 function getUserID(text) {
-  const userIDRegex = /<@U[0-9A-Z]*\>/;
   const userIDRaw = text.match(userIDRegex);
 
-  if (!userIDRaw) return null;
+  if (!userIDRaw) return "";
 
   return userIDRaw[0].slice(2, -1);
 }
 
-function getTaskText(text) {
-  const taskText = text;
-  // TODO get task text
-  return taskText;
+function resetTask() {
+  const task = Object.assign({}, emptyTask);
+}
+
+function routeErrorHandler(err, req, res, next) {
+  res.status(500);
+  res.send("error", { error: err });
+}
+
+function sendTaskToAssigned() {
+  (async () => {
+    const result = await web.conversations.open({
+      text: `Would you please: ${task.text}`,
+      users: task.assignedID,
+    });
+
+    if (result.ok) {
+      console.log("task assigned");
+    } else {
+      console.log("there was an error", result.error);
+    }
+  })();
 }
